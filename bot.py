3c1b005224792c666adb6f656e6754ea62f8f22b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-25 18:33:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
+import asyncio
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media, Media2, choose_mediaDB, db as clientDB
@@ -67,12 +68,49 @@ class Bot(Client):
         today = date.today()
         now = datetime.now(tz)
         time = now.strftime("%H:%M:%S %p")
-        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(a=today, b=time, c=temp.U_NAME))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
 
+        # Add a job to send a message at 11:59 PM daily
+        await self.send_report_message()
+    
+    async def send_report_message(self):
+        while True:
+            tz = pytz.timezone('Asia/Kolkata')
+            today = date.today()
+            now = datetime.now(tz)
+            formatted_date_1 = now.strftime("%d-%B-%Y")
+            formatted_date_2 = today.strftime("%d %b")
+            time = now.strftime("%H:%M:%S %p")
+
+            total_users = await db.total_users_count()
+            total_chats = await db.total_chat_count()
+            today_users = await db.daily_users_count(today) + 1
+            today_chats = await db.daily_chats_count(today) + 1
+
+            if now.hour == 23 and now.minute == 59:
+                await self.send_message(
+                    chat_id=LOG_CHANNEL, 
+                    text=script.REPORT_TXT.format(
+                        a=formatted_date_1,
+                        b=formatted_date_2,
+                        c=time,
+                        d=total_users, 
+                        e=total_chats,
+                        f=today_users, 
+                        g=today_chats,
+                        h=temp.U_NAME
+                    )
+                )
+                # Sleep for 1 minute to avoid sending multiple messages
+                await asyncio.sleep(60)
+            else:
+                # Sleep for 1 minute and check again
+                await asyncio.sleep(60)
+                
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
