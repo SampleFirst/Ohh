74e28223d9827586b76e06a39917bb5ebce81748
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-27 16:33:53 +0530

    Update addadmin.py

--- plugins/helper/addadmin.py
+++ plugins/helper/addadmin.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.errors import UserNotParticipant
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired
 from pyrogram.types import ChatPrivileges
 from info import ADMINS
 
@@ -17,21 +17,25 @@ async def add_channel_admin(client, message):
     chat_id = int(message.command[2])
 
     try:
-        privileges = ChatPrivileges(
-            can_change_info=True,
-            can_post_messages=True,
-            can_edit_messages=True,
-            can_delete_messages=True,
-            can_invite_users=True,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-            can_promote_members=True
+        await client.promote_chat_member(
+            chat_id,
+            user_id,
+            privileges=ChatPrivileges(
+                can_change_info=True,
+                can_post_messages=True,
+                can_edit_messages=True,
+                can_delete_messages=True,
+                can_invite_users=True,
+                can_manage_chat=True,
+                can_manage_video_chats=True,
+                can_promote_members=True
+            ),
         )
-
-        await add_admin_with_permissions(client, message, chat_id, user_id, privileges)
-
+        await message.reply("User added as an admin in the channel with specified privileges.")
     except UserNotParticipant:
         await message.reply("The user must be a member of the channel to use this command.")
+    except ChatAdminRequired:
+        await message.reply("The bot must be a admin of the channel to use this command.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
 
@@ -49,57 +53,25 @@ async def add_group_admin(client, message):
     chat_id = int(message.command[2])
 
     try:
-        privileges = ChatPrivileges(
-            can_change_info=True,
-            can_delete_messages=True,
-            can_restrict_members=True,
-            can_promote_members=True,
-            can_invite_users=True,
-            can_pin_messages=True,
-            can_manage_chat=True,
-            can_manage_video_chats=True,
-            is_anonymous=True
+        await client.promote_chat_member(
+            chat_id,
+            user_id,
+            privileges=ChatPrivileges(
+                can_change_info=True,
+                can_delete_messages=True,
+                can_restrict_members=True,
+                can_promote_members=True,
+                can_invite_users=True,
+                can_pin_messages=True,
+                can_manage_chat=True,
+                can_manage_video_chats=True
+            ),
         )
-
-        await add_admin_with_permissions(client, message, chat_id, user_id, privileges)
-
+        await message.reply("User added as an admin in the group with specified privileges.")
     except UserNotParticipant:
         await message.reply("The user must be a member of the group to use this command.")
+    except ChatAdminRequired:
+        await message.reply("The bot must be a admin of the group to use this command.")
     except Exception as e:
         await message.reply(f"An error occurred: {str(e)}")
-
-async def add_admin_with_permissions(client, message, chat_id, user_id, privileges):
-    try:
-        await client.promote_chat_member(chat_id, user_id, privileges=privileges)
-        await message.reply("User added as an admin with specified privileges.")
-    except Exception as e:
-        if "not enough rights" in str(e):
-            insufficient_permissions = []
-
-            # Check which privileges the bot doesn't have sufficient permissions for
-            if not privileges.can_change_info:
-                insufficient_permissions.append("change_info")
-            if not privileges.can_post_messages:
-                insufficient_permissions.append("post_messages")
-            if not privileges.can_edit_messages:
-                insufficient_permissions.append("edit_messages")
-            if not privileges.can_delete_messages:
-                insufficient_permissions.append("delete_messages")
-            if not privileges.can_invite_users:
-                insufficient_permissions.append("invite_users")
-            if not privileges.can_manage_chat:
-                insufficient_permissions.append("manage_chat")
-            if not privileges.can_manage_video_chats:
-                insufficient_permissions.append("manage_video_chats")
-            if not privileges.can_promote_members:
-                insufficient_permissions.append("promote_members")
-
-            # Remove insufficient permissions from privileges
-            for permission in insufficient_permissions:
-                setattr(privileges, permission, False)
-
-            await client.promote_chat_member(chat_id, user_id, privileges=privileges)
-            await message.reply(f"User added as an admin with modified privileges: {', '.join(insufficient_permissions)} skipped.")
-        else:
-            await message.reply(f"An error occurred: {str(e)}")
-            
+        
